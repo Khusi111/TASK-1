078853b03a48e9d6764233f7dbe3f61fc653b9ef
Khusi111 <swagatika.bihari2004@gmail.com>
2024-05-20 20:24:34 +0530

    TASK_1

---
+++ python1.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# creating a list 
+my_list =[1,2,3,4,5]
+
+# adding an elemrnt to the list
+my_list.append(7)
+
+# removing an element
+my_list.remove(3)
+
+#modifying
+my_list[0] = 10
+
+print("updated list:",my_list)
+
+
+# In[12]:
+
+
+# creating a dictionary
+my_dict={'name':'khusi','age':'20','city':'mumbai'}
+
+# adding 
+my_dict['gender']='male'
+
+#removing
+del my_dict['age']
+
+# modifying
+my_dict['city']='jajpur'
+
+print("updated dictionary:",my_dict)
+
+
+# In[13]:
+
+
+# creating a set
+my_set ={1,2,3,4,5}
+
+# adding
+my_set.add(8)
+
+#removing
+my_set.remove(1)
+
+# modifying
+my_set.discard(5)
+my_set.add(10)
+
+print("updated set:",my_set)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
